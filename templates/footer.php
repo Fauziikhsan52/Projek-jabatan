e0ec1411fe1b99041315fd107c496b826aafdc4b
Fauziikhsan <fauziikhsan52@gmail.com>
2025-01-07 18:32:32 +0700

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-dark text-white text-center py-4">
+        <p>&copy; 2025 Job Portal. All Rights Reserved.</p>
+    </footer>
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
